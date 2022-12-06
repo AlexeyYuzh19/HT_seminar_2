269b25f40e2b657716f2c073bbf2a1f41a781d53
AlexeyYuzh19 <9676331836@mail.ru>
2022-12-06 08:57:17 +0500

    программа по задаче 13 с цветным шрифтом

---
+++ Task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Hello, World!");
+
+System.Console.WriteLine("Введите число: ");
+int nam = Convert.ToInt32(Console.ReadLine());
+
+int nam_1 = Math.Abs(nam); 
+
+string nam_2 = Convert.ToString(nam_1);
+
+string nam_3 = (nam_1 > 99) ? ($"третья цифра введенного числа  {nam_2[2]}") : "третьей цифры в заданном числе нет";
+
+Console.ForegroundColor = ConsoleColor.Blue;
+System.Console.WriteLine(nam_3);
+Console.ResetColor();
